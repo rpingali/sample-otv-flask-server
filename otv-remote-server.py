bd45e391bd70fd23f1d28dc747c87265bcaf3eed
rohan <rohan@rpingali-f22-i7.hq.k.grp>
2016-12-03 16:41:03 -0800

    update some changes

--- otv-remote-server.py
+++ otv-remote-server.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/remote")
+def remote():
+    return "remote pressed"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
