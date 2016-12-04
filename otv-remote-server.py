2425b00d2f2aceb8b3d2af0186036f0b7273e56d
rpingali <rohan.pingali@outlook.com>
2016-12-03 18:49:15 -0800

    update irdsend

--- otv-remote-server.py
+++ otv-remote-server.py
@@ -1,13 +1,30 @@
 from flask import Flask
+from flask import request, url_for
+import subprocess
+
+
+def send_key(key):
+	try:
+		subprocess.call(
+        	["irsend", "SIMULATE", "0000000000000476",    "-00", key, "DUMMYREMOTE"])
+	except :
+		print "error in sending key"
+	return
+	
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/remote")
+@app.route("/remote/processKey")
 def remote():
-    return "remote pressed"
+	key_value = request.args.get('key', 1)
+	if key_value != None:
+		send_key(key_value)
+	return "remote pressed %s" % key_value
 
 
 if __name__ == "__main__":
